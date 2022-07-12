a099bd84bcccf51704483561d9a123a8df0cfdf3
leyi_999 <1779579409@qq.com>
2022-07-12 19:53:28 +0800

    模拟实现了 STL的list的主体架构和基本功能 且附带了重点注释
    Signed-off-by: leyi_999 <1779579409@qq.com>

---
+++ 模拟实现 stl list/test.cpp
@@ -0,0 +1,33 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include"list.hpp"
+#include<queue>
+using namespace std;
+int main() {
+	list<int> x;
+	x.push_back(1);
+	x.push_back(2);
+	x.push_back(3);
+	x.push_back(4);
+	x.push_front(0);
+	//list <int >x2(x);
+	list<int >x2(x.begin(), x.end());
+	list <int >x3(x2);
+	for (auto it = x3.begin(); it != x3.end(); ++it)
+		std::cout << *it;
+	cout << endl;
+	x.push_back(666);
+	x3 = x;
+	for (auto e : x3) {
+		cout << e;
+	}
+	cout << endl;
+	x.pop_back();
+	x.pop_front();
+	for (auto e : x) {
+		cout << e;
+	}
+	x.clear();
+
+
+	return 0;
+}
