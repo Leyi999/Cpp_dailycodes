a04090676d10ee0982e2451955f34c9e83656911
leyi_999 <1779579409@qq.com>
2022-10-08 13:13:02 +0800

    使用了initializer_list的方式拿到拓展参数，并添加了注释

--- C++ 11/可变模板参数/test.cpp
+++ C++ 11/可变模板参数/test.cpp
@@ -1,16 +1,20 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include<iostream>
+using namespace std;
 template<class T>
-void __print(const T& t) {//编译时决定 
-	std::cout << t << ' ';
+void __print( T&& t) {//编译时决定 
+	std::cout << typeid(t).name() << ' ' << t << endl;
 }
 template<class ...T>
 void print(T... args) {
 	/*if (!sizeof...(args))
 		return 0;*///不能这样写，会报错 可变模板参数展开必须在编译时就决定好
-	int arr[] = { (__print(args),0)... };
+	auto rl = { (__print(args),0)... };
+	//等价于 
+	/*initializer_list<int >il= {(__print(args), 0)...};*/
+	cout << typeid(rl).name();//利用C++ 11 initializer_list的特性依次获取参数包
 }
 int main() {
-	print("123", "321", 123);
+	print("123", "321", 0.123,999);
 	return 0;
 }
